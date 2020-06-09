d64b6e4eed98b913303233f34a2eb66912618dc3
mateusz <dalvan234@gmail.com>
2020-06-09 17:13:25 +0200

    adding new comments functionality when logged, loading screen during fetching data in choice component

--- src/app/choice/choice.component.ts
+++ src/app/choice/choice.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { PostsService } from './choice.service';
 import { HttpClient } from '@angular/common/http';
 import * as _ from 'underscore';
-
 @Component({
   selector: 'app-choice',
   templateUrl: './choice.component.html',
@@ -16,6 +15,7 @@ export class ChoiceComponent implements OnInit {
   pagesNumber;
   pages = [];
   postNames = [];
+  loaded=false;
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.http.get<any>('https://rocky-citadel-32862.herokuapp.com/Blog/posts').subscribe(data => {
@@ -28,6 +28,12 @@ export class ChoiceComponent implements OnInit {
       for(let i=1; i<this.pagesNumber+1; i++){
         this.pages.push(i);
       }
+      this.posts=this.rawPosts.slice();
+      for(let i=0; i<this.posts.length; i++){
+        this.posts[i].id=i;
+      }
+      this.loaded=true;
+      console.log(this.loaded);
     })
   }
   selectChange(){
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -19,5 +19,23 @@ export class DetailsComponent implements OnInit {
       this.post = history.state;
       this.logedUser = this.appService.getAccount();
   }
-
+  addComment(){
+    this.post[this.id].comments.push({
+      author: this.logedUser,
+      content: this.newComment,
+      id: this.post[this.id].comments.length
+    })
+    let tmpArray= this.post[this.id].comments.slice();
+    this.http.put<any>("https://rocky-citadel-32862.herokuapp.com/Blog/posts/"+this.id, {
+      title: this.post[this.id].title,
+      img: this.post[this.id].img,
+      content: this.post[this.id].content,
+      author: this.post[this.id].logedUser,
+      comments: tmpArray,
+      date: this.post[this.id].date
+      }).toPromise().then(data => {
+      console.log(data);
+      this.newComment="";
+})
+  }
 }
